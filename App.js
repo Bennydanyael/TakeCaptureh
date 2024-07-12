4443864f5e9ce8aecfed5a3c74b40fc7a3f452f4
Benni Daniel Silaban <benni.silaban@sakura-system.co.id>
2024-07-12 17:15:20 +0700

    Update, Feat : Take Screenshoot - Expo.

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
+import * as MediaLibrary from 'expo-media-library';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { captureRef } from 'react-native-view-shot';
+// import { useState, useRef } from 'react';
+
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  // const imageRef = useRef();
+  // const [status, requestPermission] = MediaLibrary.usePermissions();
+
+  if (status === null) {
+    requestPermission();
+  }
+
+  const onSaveImageAsync = async () => {
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        alert("Saved!");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    // <GestureHandlerRootView style={styles.container}>
+     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+      <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+      <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
+      /* <View style={styles.imageContainer}>
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
+          {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+        </View>
+      </View>
+    </GestureHandlerRootView> */
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
